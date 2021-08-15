17da0bbf289a5c1c0e11561ee1b64df01ee2f4e6
Prashantha T P <tpprashantha@gmail.com>
2021-08-16 01:07:59 +0530

    feat: 🎸 build draft of landing page component
    add typography and Link

--- src/containers/salvin/home.js
+++ src/containers/salvin/home.js
@@ -1,10 +1,66 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { Container, Box, Typography, Button, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Logo from "../../assets/images/logo.PNG";
+import { componentsRoutes } from "../../common/routes";
+const homeStyles = makeStyles({
+  centered: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    minHeight: "100%",
+    minWidth: "100%",
+  },
+  cover: {
+    height: "100%",
+    width: "100%",
+  },
+  wordcase: {
+    textTransform: "capitalize",
+    fontWeight: "bold",
+  },
+  uppercase: {
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
 const Home = () => {
+  const classes = homeStyles();
+
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <Container className={classes.cover}>
+      <Box className={classes.centered}>
+        <Paper variant="outlined">
+          <img src={Logo} alt="Salvin Instruments Logo" />
+        </Paper>
+
+        <Typography
+          variant="h2"
+          component="h2"
+          display="block"
+          gutterBottom
+          className={classes.uppercase}
+        >
+          csr csi
+        </Typography>
+        <Typography
+          variant="h4"
+          component="h4"
+          display="block"
+          gutterBottom
+          className={classes.wordcase}
+        >
+          user manual
+        </Typography>
+        <Link to={componentsRoutes.home.path}>
+          <Button variant="contained" color="primary">
+            Start
+          </Button>
+        </Link>
+      </Box>
+    </Container>
   );
 };
 
